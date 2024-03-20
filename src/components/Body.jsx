559c064d8017a86e22d9da49dacaa74254587568
Deborah Kudaru <deborahkudaru@gmail.com>
2024-03-20 19:40:37 +0100

    chnages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "../images/logo.png";
 import Header from "./Header";
-
+import "./styles/styles.css"
 
 const Body = () => {
   return (
-    <div className="bg-sza bg-no-repeat bg-contain md:bg-cover md:bg-center bg-fixed bg-black bg-opacity-70 bg-blend-overlay">
+    <div className=" bg-no-repeat bg-contain md:bg-cover md:bg-center bg-fixed bg-black bg-opacity-70 bg-blend-overlay background">
       <Header />
       <img className="md:w-96 md:m-auto md:py-48  w-44 m-auto pt-44 pb-10 " src={logo} alt="Quan Imagery" />
      
