72b7fbcc744c3f2ee7f7a9554ff6b088dfb3c71a
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-09-29 14:50:29 +0100

    fix: correct import statement for Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import "styles/Loader.css"; // Create this CSS file for your loader styles
+import "../styles/Loader.css"; // Create this CSS file for your loader styles
 
 const Loader = () => (
   <div className="loader-container">
